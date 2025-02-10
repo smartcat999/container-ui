0527deba3acc7b4fbdeeacf2e523b92e6dffe6ac
peng wu <2030047311@qq.com>
2025-02-11 00:22:35 +0800

    feat: support local dev proxy
    Signed-off-by: peng wu <2030047311@qq.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -12,7 +13,17 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': resolve(__dirname, 'src')
     },
   },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        ws: true  // 支持 websocket
+      }
+    }
+  }
 })
